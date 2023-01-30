cc9d71b4c3166f9336ea54647251fe2002325188
kelsieszost <owner@kelsieszost.design>
2023-01-29 20:27:43 -0500

    added camera and light, sphere visible on screen

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+camera.position.z = 20
+scene.add(camera)
+
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600)
+renderer.render(scene, camera)
+
