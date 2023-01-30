8db6135a4b7170c7265a414e42cb9947eede2653
kelsieszost <owner@kelsieszost.design>
2023-01-29 21:27:17 -0500

    added orbit controls/loop/responsiveness

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import './style.css'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 //Scene
 const scene = new THREE.Scene();
@@ -6,14 +8,20 @@ const scene = new THREE.Scene();
 //Sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-  color: '#00ff83',
+  color: '#D81DE3',
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 
-const camera = new THREE.PerspectiveCamera(45, 800 / 600)
-camera.position.z = 20
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 100)
+camera.position.z = 15
 scene.add(camera)
+
+
 
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0, 10, 10)
@@ -21,6 +29,28 @@ scene.add(light)
 
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+window.addEventListener('resize', () => {
+  //update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //udpdate camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+  // mesh.rotation.x += 0.1
+  controls.update()
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+}
+loop()
